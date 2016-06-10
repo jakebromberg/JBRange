ac578478cbc31b3882a153c21ab3f52d273e1474
Jake Bromberg <jakebromberg@nestlabs.com>
2016-06-10 13:52:36 -0700

    Initial Commit

---
+++ JBRangeLib/JBRangeLib.h
@@ -0,0 +1,19 @@
+//
+//  JBRangeLib.h
+//  JBRangeLib
+//
+//  Created by jakebromberg on 6/10/16.
+//  Copyright © 2016 jakebromberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JBRangeLib.
+FOUNDATION_EXPORT double JBRangeLibVersionNumber;
+
+//! Project version string for JBRangeLib.
+FOUNDATION_EXPORT const unsigned char JBRangeLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBRangeLib/PublicHeader.h>
+
+
