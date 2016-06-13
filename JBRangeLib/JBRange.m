8b8e8b72e6d103f66f2e43e732afe59792c3322d
Jake Bromberg <jakebromberg@nestlabs.com>
2016-06-12 23:13:18 -0700

    Now storing the index ivars in terms of NSNumbers, not NSIntegers. We can avoid offset conversions now in our enumeration method

--- JBRangeLib/JBRange.m
+++ JBRangeLib/JBRange.m
@@ -17,16 +17,25 @@
 
 @implementation JBRange
 
+static unsigned long zero;
+static unsigned long unitDistance;
+
 - (instancetype)initWithStartIndex:(NSInteger)startIndex endIndex:(NSInteger)endIndex {
     if (startIndex > endIndex) {
         return nil;
     }
     
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        zero = (unsigned long)(__bridge void *)(@0);
+        unitDistance = (unsigned long)(__bridge void *)(@1) - zero;
+    });
+
     self = [super init];
     
     if (self != nil) {
-        _i_startIndex = startIndex;
-        _i_endIndex = endIndex;
+        _i_startIndex = startIndex * unitDistance + zero;
+        _i_endIndex = endIndex * unitDistance + zero;
     }
     
     return self;
@@ -35,31 +44,26 @@
 #pragma mark NSFastEnumeration
 
 - (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state objects:(__unsafe_unretained id  _Nonnull *)buffer count:(NSUInteger)len {
-    static void *zero;
-    static char distance;
-    
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        zero = (__bridge void *)(@0);
-        distance = (__bridge void *)(@1) - zero;
-    });
     
     if (state->extra[1] == 0) {
         state->extra[1] = 1;
-        state->state = _i_startIndex * distance + zero;
+        state->state = _i_startIndex;
         state->mutationsPtr = state->extra;
         state->itemsPtr = buffer;
     }
     
-    if ((_i_endIndex * distance + zero) <= state->state) {
+    if (_i_endIndex <= state->state) {
         return 0;
     }
 
-    NSInteger numIterations = MIN((_i_endIndex * distance + zero) - state->state, len);
+    NSInteger numIterations = MIN(_i_endIndex - state->state, len);
+    NSInteger maxIndex = state->state + numIterations * unitDistance;
     
-    for (NSInteger i = 0; i < numIterations; state->state+=distance, i++) {
-        *buffer++ = (id _Nonnull)(state->state);
+    for (NSInteger i = state->state; i < maxIndex; i += unitDistance) {
+        *buffer++ = (id _Nonnull)i;
     }
+    
+    state->state = maxIndex;
     
     return numIterations;
 }
@@ -79,17 +83,19 @@
 #pragma mark NSCopying
 
 - (id)copyWithZone:(NSZone *)zone {
-    return [[JBRange allocWithZone:zone] initWithStartIndex:_i_startIndex endIndex:_i_endIndex];
+    NSInteger startIndex = (_i_startIndex - zero) / unitDistance;
+    NSInteger endIndex = (_i_endIndex - zero) / unitDistance;
+    return [[JBRange allocWithZone:zone] initWithStartIndex:startIndex endIndex:endIndex];
 }
 
 #pragma mark Properties
 
 - (NSNumber *)startIndex {
-    return @(_i_startIndex);
+    return (id)_i_startIndex;
 }
 
 - (NSNumber *)endIndex {
-    return @(_i_endIndex);
+    return (id)_i_endIndex;
 }
 
 @end
